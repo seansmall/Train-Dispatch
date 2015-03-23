a410b298aa8012548a87e77faffab4479f775d64
oojutiku <oojutiku2013@my.fit.edu>
2015-03-23 16:36:18 -0400

    Create Train.java
    First Draft. Let me know if there's anything to change. Just a constructor with get methods

---
+++ Train.java
@@ -0,0 +1,31 @@
+
+public class Train {
+
+    private int source, destination, time;
+    private String type;
+    public Train (int s, int d, int t) {
+        source = s;
+        destination = d;
+        time = d;
+    }
+    
+    public int getSource () {
+        return source;
+    }
+    
+    public int getDestination () {
+        return destination;
+    }
+    
+    public int getProposedTime () {
+        return time;
+    }
+    
+    public int getDelay (int actualtime) {
+        return actualtime - time;
+    }
+    
+    public void setType (String t) {
+        type = t;
+    }
+}
