c05096aca0c7efef841b74ad76903aacd2b5afee
anthony0011 <cesposito2014@my.fit.edu>
2015-03-29 17:46:24 -0400

    vertex class for the graph

---
+++ Vertex.java
@@ -0,0 +1,65 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Vertex implements Comparable<Vertex> {
+
+	private final String ID;
+	private final int vertexNumber;
+	private Vertex prev;
+	private int distance = Integer.MAX_VALUE;
+	private final Map<Vertex, Integer> adj = new HashMap<>();
+	
+	public Vertex(final String id, final int vertexNum) {
+		this.ID = id;
+		this.vertexNumber = vertexNum;
+	}
+	
+	public String getPath() {
+		
+		String path = "";
+		
+		if (this == this.prev) {
+			path = this.ID + "(" + this.distance +") ";
+		} else if (this.prev == null) {
+			path = "unreached";
+		} else {
+			path = path + this.prev.getPath() + this.ID + "(" + this.distance + ") ";
+		}
+		
+		return path;
+	}
+	
+	@Override
+	public int compareTo(Vertex vertex) {
+		return Integer.compare(vertexNumber, vertex.getVertexNumber());
+	}
+
+	public Vertex getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Vertex vertex) {
+		this.prev = vertex;
+	}
+
+	public String getID() {
+		return ID;
+	}
+
+	public int getVertexNumber() {
+		return vertexNumber;
+	}
+
+	public Map<Vertex, Integer> getAdj() {
+		return adj;
+	}
+
+	public int getDistance() {
+		return distance;
+	}
+
+	public void setDistance(int distance) {
+		this.distance = distance;
+	}	
+}
