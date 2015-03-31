61581cac070d0c580bc23c13228b428f0ff166bf
anthony0011 <cesposito2014@my.fit.edu>
2015-03-31 16:31:12 -0400

    update to Vertex.java
    fixed a bug with the compareTo method

--- Vertex.java
+++ Vertex.java
@@ -4,62 +4,62 @@ import java.util.Map;
 
 public class Vertex implements Comparable<Vertex> {
 
-	private final String ID;
-	private final int vertexNumber;
-	private Vertex prev;
-	private int distance = Integer.MAX_VALUE;
-	private final Map<Vertex, Integer> adj = new HashMap<>();
-	
-	public Vertex(final String id, final int vertexNum) {
-		this.ID = id;
-		this.vertexNumber = vertexNum;
-	}
-	
-	public String getPath() {
-		
-		String path = "";
-		
-		if (this == this.prev) {
-			path = this.ID + "(" + this.distance +") ";
-		} else if (this.prev == null) {
-			path = "unreached";
-		} else {
-			path = path + this.prev.getPath() + this.ID + "(" + this.distance + ") ";
-		}
-		
-		return path;
-	}
-	
-	@Override
-	public int compareTo(Vertex vertex) {
-		return Integer.compare(vertexNumber, vertex.getVertexNumber());
-	}
+    private final String ID;
+    private final int vertexNumber;
+    private Vertex prev;
+    private int distance = Integer.MAX_VALUE;
+    private final Map<Vertex, Integer> adj = new HashMap<>();
+    
+    public Vertex(final String id, final int vertexNum) {
+        this.ID = id;
+        this.vertexNumber = vertexNum;
+    }
+    
+    public String getPath() {
+        
+        String path = "";
+        
+        if (this == this.prev) {
+            path = this.ID + "(" + this.distance +") ";
+        } else if (this.prev == null) {
+            path = "unreached";
+        } else {
+            path = path + this.prev.getPath() + this.ID + "(" + this.distance + ") ";
+        }
+        
+        return path;
+    }
+    
+    @Override
+    public int compareTo(Vertex vertex) {
+        return Integer.compare(distance, vertex.getDistance());
+    }
 
-	public Vertex getPrev() {
-		return prev;
-	}
+    public Vertex getPrev() {
+        return prev;
+    }
 
-	public void setPrev(Vertex vertex) {
-		this.prev = vertex;
-	}
+    public void setPrev(Vertex vertex) {
+        this.prev = vertex;
+    }
 
-	public String getID() {
-		return ID;
-	}
+    public String getID() {
+        return ID;
+    }
 
-	public int getVertexNumber() {
-		return vertexNumber;
-	}
+    public int getVertexNumber() {
+        return vertexNumber;
+    }
 
-	public Map<Vertex, Integer> getAdj() {
-		return adj;
-	}
+    public Map<Vertex, Integer> getAdj() {
+        return adj;
+    }
 
-	public int getDistance() {
-		return distance;
-	}
+    public int getDistance() {
+        return distance;
+    }
 
-	public void setDistance(int distance) {
-		this.distance = distance;
-	}	
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }   
 }
