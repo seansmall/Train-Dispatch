4b89ee0897c27bfb1d2171b4a913b46b08671a2f
anthony0011 <cesposito2014@my.fit.edu>
2015-04-07 16:53:10 -0400

    Update Vertex.java
    should be done just need to add comments when I can

--- Vertex.java
+++ Vertex.java
@@ -4,158 +4,56 @@ import java.util.Map;
 
 public class Vertex implements Comparable<Vertex> {
 
-    private final String ID;
-    private final int vertexNumber;
-    private Vertex prev;
-    private int distance = Integer.MAX_VALUE;import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
+	private final String ID;
+	private Vertex prev;
+	private int distance = Integer.MAX_VALUE;
+	private final Map<Vertex, Integer> adj = new HashMap<>();
+	
+	public Vertex(final String id) {
+		this.ID = id;
+	}
+	
+	public String getPath() {
+		
+		String path = "";
+		
+		if (this == this.prev) {
+			path = this.ID + " " + this.distance +" ";
+		} else if (this.prev == null) {
+			path = "unreached";
+		} else {
+			path = path + this.prev.getPath() + this.ID + " " + this.distance + " ";
+		}
+		
+		return path;
+	}
+	
+	@Override
+	public int compareTo(Vertex vertex) {
+		return Integer.compare(distance, vertex.getDistance());
+	}
 
+	public Vertex getPrev() {
+		return prev;
+	}
 
-public class Vertex implements Comparable<Vertex> {
+	public void setPrev(Vertex vertex) {
+		this.prev = vertex;
+	}
 
-    private final String ID;
-    private final int vertexNumber;
-    private Vertex prev;
-    private int distance = Integer.MAX_VALUE;
-    private final Map<Vertex, Integer> adj = new HashMap<>();
-    private Type type;
-    
-    enum Type {
-        station,
-        intermediatePoint
-    }
-    
-    public Vertex(final String id, final int vertexNum) {
-        this.ID = id;
-        this.vertexNumber = vertexNum;
-    }
-    
-    public LinkedList<int[]> getPath() {
-        
-        LinkedList<int[]> path = new LinkedList<int[]>();
-        
-        if (this == this.prev) {
-            int[] station = {this.vertexNumber, this.distance};
-            path.add(station);
-        } else if (this.prev == null) {
-            path = null;
-        } else {
-            path.addAll(this.prev.getPath());
-            int[] station = {this.vertexNumber, this.distance};
-            path.add(station);
-        }
-        
-        return path;
-    }
-    
-    @Override
-    public int compareTo(Vertex vertex) {
-        return Integer.compare(distance, vertex.getDistance());
-    }
+	public String getID() {
+		return ID;
+	}
 
-    public Vertex getPrev() {
-        return prev;
-    }
+	public Map<Vertex, Integer> getAdj() {
+		return adj;
+	}
 
-    public void setPrev(Vertex vertex) {
-        this.prev = vertex;
-    }
+	public int getDistance() {
+		return distance;
+	}
 
-    public String getID() {
-        return ID;
-    }
-
-    public int getVertexNumber() {
-        return vertexNumber;
-    }
-
-    public Map<Vertex, Integer> getAdj() {
-        return adj;
-    }
-
-    public int getDistance() {
-        return distance;
-    }
-
-    public void setDistance(int distance) {
-        this.distance = distance;
-    }
-
-    public Type getType() {
-        return type;
-    }
-
-    public void setType(Type type) {
-        this.type = type;
-    }
-}
-    private final Map<Vertex, Integer> adj = new HashMap<>();
-    private Type type;
-    
-    enum Type {
-        station,
-        intermediatePoint
-    }
-    
-    public Vertex(final String id, final int vertexNum) {
-        this.ID = id;
-        this.vertexNumber = vertexNum;
-    }
-    
-    public String getPath() {
-        
-        String path = "";
-        
-        if (this == this.prev) {
-            path = this.ID + "(" + this.distance +") ";
-        } else if (this.prev == null) {
-            path = "unreached";
-        } else {
-            path = path + this.prev.getPath() + this.ID + "(" + this.distance + ") ";
-        }
-        
-        return path;
-    }
-    
-    @Override
-    public int compareTo(Vertex vertex) {
-        return Integer.compare(distance, vertex.getDistance());
-    }
-
-    public Vertex getPrev() {
-        return prev;
-    }
-
-    public void setPrev(Vertex vertex) {
-        this.prev = vertex;
-    }
-
-    public String getID() {
-        return ID;
-    }
-
-    public int getVertexNumber() {
-        return vertexNumber;
-    }
-
-    public Map<Vertex, Integer> getAdj() {
-        return adj;
-    }
-
-    public int getDistance() {
-        return distance;
-    }
-
-    public void setDistance(int distance) {
-        this.distance = distance;
-    }
-    
-    public Type getType() {
-        return type;
-    }
-
-    public void setType(Type type) {
-        this.type = type;
-    }
+	public void setDistance(int distance) {
+		this.distance = distance;
+	}	
 }
