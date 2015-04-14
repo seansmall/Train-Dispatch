a86f0d2f81d2c52c56e87439738ea04150f7c6bd
anthony0011 <cesposito2014@my.fit.edu>
2015-04-14 15:28:04 -0400

    Update Schedule.java
    conflict check working properly now, trains in conflict will not leave until conflict resolved ie current moving train reached its destination

--- Schedule.java
+++ Schedule.java
@@ -8,65 +8,65 @@ public class Schedule {
     
     private LinkedList<Train> runningList;
 
-	public Schedule() {
-    	runningList = new LinkedList<Train>();
+    public Schedule() {
+        runningList = new LinkedList<Train>();
     }
     
     public boolean add(final Train t) {
-    	
-    	boolean addSuccessful = true;
-    	
-    	for (Train train : runningList) {
-    		if (conflict(t, train)) {
-    			addSuccessful = false;
-    			break;
-    		}
-    	}
-    	
-    	runningList.add(t);
-    	t.running();
-    	return addSuccessful;
+        
+        boolean addSuccessful = true;
+        
+        for (Train train : runningList) {
+            if (conflict(t, train)) {
+                addSuccessful = false;
+                break;
+            }
+        }
+
+        if (addSuccessful) {
+        	runningList.add(t);
+        	t.running();
+        }
+        return addSuccessful;
     }
 
     public boolean conflict(final Train trainOne, final Train trainTwo) {
-    	LinkedList<Edge> routeOne = trainOne.getRoute();
-    	LinkedList<Edge> routeTwo = trainTwo.getRoute();
-    	
-    	boolean conflict = false;
+        LinkedList<Edge> routeOne = trainOne.getRoute();
+        LinkedList<Edge> routeTwo = trainTwo.getRoute();
 
-		for (Edge edgeOne : routeOne) {
-			for (Edge edgeTwo : routeTwo) {
-				if (edgeOne.equals(edgeTwo)) {
-					conflict = true;
-					System.out.println("conflit between train " + trainOne.getID() + " and " + trainTwo.getID() + " at " + edgeOne.toString());
-				}
-			}
-		}
-    	return conflict;
+        for (Edge edgeOne : routeOne) {
+            for (Edge edgeTwo : routeTwo) {
+                if (edgeOne.equals(edgeTwo)) {
+                    //System.out.println("conflit between train " + trainOne.getID() + " and " + trainTwo.getID() + " at " + edgeOne.toString());
+                    return true;
+                }
+            }
+        }
+        return false;
     }
     
     public void updateTrains(final int time) {
 
-    	for (Train train : runningList) {
-    		
-    		train.move();
-    		
-    		if (train.getLastStation().equals(train.getDestName())) {
-       			train.park();
-    			train.setArrived(true);
-    			train.setActualArrivalTime(time);
-    			runningList.remove(train);
-    			System.out.println( "train " + train.getID() + " has arrived at " + train.getActualArrivalTime() + " expected arrival was at " + train.getExpectedArrivalTime());
-    			System.out.println("train " + train.getID() + " took route " + train.getRoute().toString() + "\r");
-    		}
-    	}    	
+        for (Train train : runningList) {
+            
+            train.move();
+            
+            if (train.getLastStation().equals(train.getDestName())) {
+                train.park();
+                train.setArrived(true);
+                train.setActualArrivalTime(time + 1);
+                runningList.remove(train);
+                System.out.println( "train " + train.getID() + " has arrived at " + train.getActualArrivalTime() + " expected arrival was at " + train.getExpectedArrivalTime());
+                System.out.println("train " + train.getID() + " took route " + train.getRoute().toString() + "\r");
+            }
+        }       
     }
     
     public LinkedList<Train> getRunningList() {
-		return runningList;
-	}
+        return runningList;
+    }
 
-	public void setRunningList(LinkedList<Train> runningList) {
-		this.runningList = runningList;
-	}
+    public void setRunningList(LinkedList<Train> runningList) {
+        this.runningList = runningList;
+    }
 }
