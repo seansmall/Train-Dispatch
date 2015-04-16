5b0228f00aa51b12526343681653acb9399bab8b
oojutiku <oojutiku2013@my.fit.edu>
2015-04-16 16:19:58 -0400

    Simple Helper constructor

---
+++ Coordinates.java
@@ -0,0 +1,24 @@
+
+public class Coordinates {
+
+    private double x,y;
+    public Coordinates (double a, double b) {
+        x = a;
+        y = b;
+    }
+   public double getX() {
+       return x;
+   }
+   
+   public double getY() {
+       return y;
+   }
+   
+   public void setX(double a) {
+       x = a;
+   }
+   
+   public void setY(double a) {
+       y = a;
+   }
+}
