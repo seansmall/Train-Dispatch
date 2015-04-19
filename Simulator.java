0bebbcee3363b24f70a3e6e25147934179ffcd50
seansmall <s.swe@icloud.com>
2015-04-19 15:24:45 -0400

    Update Simulator.java
    Save Train Info function added

--- Simulator.java
+++ Simulator.java
@@ -30,6 +30,35 @@ public class Simulator {
         System.out.println("Test Case Delay: " + test.getDelaySum());
     }
     
+    public static void saveTrainInfo (ArrayList<Train> sequence,
+			String fileName) throws FileNotFoundException,
+			UnsupportedEncodingException {		
+		
+		// checks if filename already exists
+		// and adds an index if it does
+		File f = new File(fileName + ".txt");
+		String newName;
+		if(f.exists() && !f.isDirectory()) {
+			int count = 1;
+			do {
+				newName = fileName + "(" + count + ")";
+				f = new File(newName + ".txt");
+				count++;
+			} while (f.exists() && !f.isDirectory());
+		}
+		// saves file to directory
+		PrintWriter writer = new PrintWriter(f, "UTF-8");
+		
+		for (Train train : sequence) {
+			String s = String.format("%-5.5s", train.getID() + ",");
+			String d = String.format("%-1.3s", train.getSpeed() + ",");
+			String dt = String.format("%7.7s", train.getActualDepatureTime());
+			writer.print(s + d + dt);
+			writer.println(train.getRoute());
+		}
+		writer.close();
+	}
+    
     public static ArrayList<Train> readSequence(final File fileName) {
         
         ArrayList<Train> seq = new ArrayList<Train>();
