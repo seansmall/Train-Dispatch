e8a8576687ba4afdf98a750bd43d8c6776a4f547
anthony0011 <cesposito2014@my.fit.edu>
2015-04-20 18:45:26 -0400

    Update SmartController.java
    same as controller

--- SmartController.java
+++ SmartController.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public class SmartController extends Controller {
 
-	private SmartSchedule runningTrains;
+	private SmartSchedule runningTrains;						// smart schedule of running trains
 	
 	public SmartController(Graph g, ArrayList<Train> sequence) {
 		super(g, sequence);
@@ -13,44 +13,40 @@ public class SmartController extends Controller {
 	@Override
     public void update() {
         
-        for (Train train : sequence) {
-            if ((!train.hasArrived()) && (train.getDepatureTime() <= ticks) && (!train.isMoving()) && (!runningTrains.getRunningList().contains(train))) {
+		for (Train train : sequence) {
+			// check if the current train is valid for departure
+            if ((!train.hasArrived()) && (train.getDepatureTime() <= ticks) && (!runningTrains.getRunningList().contains(train))) {
+            	// try to add the train to the schedule
                 if (runningTrains.add(train, ticks)) {
-                    //TODO fix departure time with a delay
+                	// if successful increment the running train count and set the actual departure time
                     System.out.println( "train " + train.getID() + " has departed at " + ticks + " expected depature time was at " + train.getDepatureTime() + "\r");
                     numTrainsRunning++;
+                    runningTrains.setTrainsMoving(runningTrains.getTrainsMoving() + 1);
                     train.setActualDepartureTime(ticks);
+                    System.out.println(numTrainsRunning);
                 }
-            }
-            // if the train reaches the delay station then it should stop moving until after the delay time
-            // has passed
-            if ((!train.hasArrived()) && (runningTrains.getRunningList().contains(train))) {
-                if ((train.getDelayStop().equals(train.getLastStation())) && (train.getDelayTime() > ticks)) {
-                    train.park();
-                    //System.out.println(" at " + ticks + " train " + train.getID() + " will delay at " + train.getDelayStop() + " until " + train.getDelayTime());
-                } else if ((train.getDelayStop().equals(train.getLastStation())) && (train.getDelayTime() < ticks)) {
-                    train.running();
-                    //System.out.println(" at " + ticks + " train " + train.getID() + " resumed running from " + train.getDelayStop());
-                }
-            }
-        }
-        runningTrains.updateTrains(ticks);
-        ticks++;
-    }
-
-	@Override
-    public void updateTrainsRunning() {
-        for (Train train : sequence) {
-            if ((!train.hasArrived()) && (train.getDepatureTime() <= ticks) && (!train.isMoving())) {
-                if (runningTrains.add(train)) {
-                    System.out.println( "train " + train.getID() + " has departed at " + ticks + " expected depature time was at " + train.getDepatureTime() + "\r");
-                    numTrainsRunning++;
-                    train.setActualDepartureTime(ticks);
-                }
-            } else if (train.hasArrived()) {
+            // the train has arrived remove it from the schedule and decrement the running train count
+            } else if (train.hasArrived() && (runningTrains.getRunningList().contains(train))) {
                 runningTrains.remove(train);
                 numTrainsRunning--;
+                //System.out.println(numTrainsRunning);
+            }
+           // System.out.println(runningTrains.getTrainsDelaying()  + " , " + runningTrains.getTrainsMoving());
+            // re-calculate delays in case of dead lock
+            if ((runningTrains.getRunningList().size() == runningTrains.getTrainsDelaying()) && (runningTrains.getRunningList().contains(train))) {
+            	runningTrains.getRunningList().clear();
+            	numTrainsRunning = 0;
+            	runningTrains.setTrainsDelaying(0);
+            	runningTrains.setTrainsMoving(0);
+            	if (runningTrains.add(train, ticks)) {
+            		 runningTrains.setTrainsMoving(runningTrains.getTrainsMoving() + 1);
+            		 numTrainsRunning++;
+            	}
             }
         }
+		// update the schedule
+        runningTrains.updateTrains(ticks);
+        // increment time
+        ticks++;
     }
 }
